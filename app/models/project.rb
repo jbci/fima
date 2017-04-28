1d1bf0c5417754b58ada5541fe885ae6aa2af5f4
jbci <jbari@ciudadanoineligente.org>
2017-04-28 18:26:23 -0300

    Show carousel in citizen_obs

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,5 +3,9 @@ class Project < ApplicationRecord
 
   has_attached_file :image
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
-  
+
+  def remaining_days
+    (self.end_date - Date.today).to_i
+  end
+
 end
