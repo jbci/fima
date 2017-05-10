f5b5e61aeda600c00b7280d6165f0a04a702ed76
jbci <jbari@ciudadanoineligente.org>
2017-05-10 16:58:28 -0300

    fix google oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,12 +1,1 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET'],
-    {
-      :name => "google",
-      :scope => "email, profile",
-      :prompt => "select_account",
-      :image_aspect_ratio => "square",
-      :image_size => 50
-    }
-end
-
 OmniAuth.config.full_host = Rails.env.production? ? 'https://' + ENV['BASE_URL'] : ENV['BASE_URL'] + ":" + ENV['BASE_URL_PORT']
